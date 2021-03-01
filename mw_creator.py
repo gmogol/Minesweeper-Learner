13e18541187a5a44cf562ab5c0b9744e99b7f9eb
[Gonenc Mogol] <gonencmogol@gmail.com>
2021-03-01 11:25:42 -0500

    Added documentation and comments

--- mw_creator.py
+++ mw_creator.py
@@ -5,7 +5,8 @@ class Minesweper:
     def __init__(self,size,num_mine):
         """
         Creates a square board of Minesweper of a given size and
-        number of mines
+        number of mines. The conventions for the board is as follows:
+        Any integer >= 0 is a number and mines are encoded as -1
         """
         self.size = size
         self.num_mine = num_mine
@@ -18,7 +19,7 @@ class Minesweper:
         
 
     @property
-    def board(self):
+    def board(self):            # creates the board once and for all
         if self._board is None:
             self._board = self.minesweper(self.size,self.num_mine)
             return self._board
@@ -26,6 +27,10 @@ class Minesweper:
 
 
     def add_nghb(self,row,col,mines):
+        """
+        Given a mine position updates its neighbors so that the
+        numbers on the board are created and are correct
+        """
         size = self.size
         prev_row = max(0,row-1)
         prev_col = max(0,col-1)
@@ -39,6 +44,10 @@ class Minesweper:
         
 
     def minesweper(self,size,num_mine):
+        """
+        The main function that creates the board given a size and
+        number of mines
+        """
         if num_mine > size**2:
             raise Exception('Too many mines')
         mines = np.zeros([size,size],dtype = int)
@@ -62,7 +71,15 @@ class Minesweper:
 class Player():
     def __init__(self,mw):
         """
-
+        This is the class to "play" the game. It takes a Minesweper board
+        as the only argument. The conventions are as follows:
+        The board is stored as a string array.
+        - denotes an unopened cell
+        x denotes a flagged cell
+        ? denotes a question-flagged cell
+        We further count the number of moves that the player plays
+        this includes opening 0 cells. So a player who wins the game will have
+        done at least size**2 moves. 
         """
         self.mw = mw
         size = mw.size
@@ -82,6 +99,7 @@ class Player():
         
         
     def open(self,row,col):
+        """ Open the cell row,col """
         size = self.size
         if self.board[row,col] in self.playable:
             self.num_moves += 1
@@ -93,7 +111,10 @@ class Player():
             self.board[row,col] = str(val)
             
             if self.mw.board[row,col] == 0:
-                
+                # When you open an empty cell
+                # all the neighboring cells should be opened
+                # this is probably not the most efficient way of doing this
+                # but for a smaller board sizes it should be just fine
                 prev_row = max(0,row-1)
                 prev_col = max(0,col-1)
                 next_row = min(size-1, row+1)
@@ -113,6 +134,7 @@ class Player():
             
 
     def flag(self,row,col):
+        """ Flag the cell as a mine """ 
         self.tot_moves += 1
         if self.board[row,col] in self.playable:
 
@@ -121,12 +143,14 @@ class Player():
         return self.board
 
     def flag_q(self,row,col):
+        """ Flags it as a question mark """
         self.tot_moves += 1
         if self.board[row,col] in self.playable:
             self.board[row,col] = '?'
         return self.board
 
     def unflag(self,row,col):
+        """ Unflags the cell """ 
         self.tot_moves += 1
         if self.board[row,col] in ['x','?']:
             self.board[row,col] = '-'
